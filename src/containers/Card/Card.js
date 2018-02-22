61dd39036b8ca883232c97e986c68610de67878f
Natalie Montoya <nataliesbarron@gmail.com>
2018-02-22 11:58:55 -0700

    create card component

---
+++ src/containers/Card/Card.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Card extends Component {
+ 
+  render() {
+    return (
+      <div>
+        <h3>Cards</h3>
+      </div>
+    )
+  }
+}
+
+export default Card;
